58dd2370e7025965925f82ec482fb2e3f6f257e1
Shakurri <puz,show40716@gmail.com>
2019-10-25 15:07:01 +0900

    caption

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+$(function(){
+	var lordF=0;
 
 
 
+  $('.howtoButton').on('touchstart',function () {
+  	if(lordF==0){
+  	$('.slider').slick({
+    autoplay:false,
+    arrows:true,
+
+    dots:true,
+  	});
+  	lordF++;
+  	}
+  	$('.caption').fadeIn(500);
+  	$('.slider').fadeIn(1000);
+  });
+  $('.batu').on('touchstart',function () {
+  	$('.caption').fadeOut(1000);
+  	$('.slider').fadeOut(500);
+  });
+});
